f6539c7cb3341d1dba320c832e576cec202ef36f
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-10-23 13:57:03 +0900

    초기 세팅

---
+++ src/views/main/ui/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@/shared/lib/cn";
+
+const HomePage = () => {
+  return (
+    <div className={cn("flex", "bg-red-500", "w-screen", "h-screen")}>d</div>
+  );
+};
+
+export default HomePage;
