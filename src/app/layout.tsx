fbc39f623bc65fc75ca805488ee5651a7a902609
bom0320 <s24040@gsm.hs.kr>
2026-03-24 14:04:06 +0900

    feat: add toast (sonner) setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "@/shared/styles/globals.css";
 import TanStackProvider from "@/shared/lib/TanStackProvider";
-
+import { Toaster } from "sonner";
 export const metadata: Metadata = {
   title: "washer",
   description: "광주소프트웨어마이스터고 세탁건조기 관리 서비스",
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <TanStackProvider>{children}</TanStackProvider>
+        <Toaster position="top-right" closeButton richColors />
       </body>
     </html>
   );
