2daa746b7cdfa65b8e11dbc17a9a8b1604c7f02c
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-07 15:09:09 +0900

    feat: 토큰 만료시 재발급

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,22 +1,94 @@
 import axios from "axios";
+import { COOKIE_KEYS } from "../constants/cookies";
+import { getCookie, setCookie } from "../utils/cookies";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/",
 });
 
-axiosInstance.interceptors.request.use(
-  async (config) => {
-    return config;
-  },
-  async (error) => Promise.reject(error),
-);
+type RefreshResponse = {
+  data: {
+    accessToken: string;
+    expiresIn: number;
+    refreshToken: string;
+  };
+};
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = getCookie(COOKIE_KEYS.ACCESS_TOKEN);
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+const onTokenRefreshed = (token: string) => {
+  refreshSubscribers.forEach((callback) => {
+    callback(token);
+  });
+  refreshSubscribers = [];
+};
+
+const addRefreshSubscriber = (callback: (token: string) => void) => {
+  refreshSubscribers.push(callback);
+};
 
 axiosInstance.interceptors.response.use(
   (response) => {
-    if (response.status >= 200 && response.status <= 300) {
-      return response.data;
-    }
-    return Promise.reject(response.data);
+    return response.data;
   },
-  async (error) => Promise.reject(error),
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = getCookie(COOKIE_KEYS.REFRESH_TOKEN);
+
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          addRefreshSubscriber((token: string) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(axiosInstance(originalRequest));
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const response: RefreshResponse = await axiosInstance.post(
+          "/api/v2/auth/refresh",
+          {
+            refreshToken,
+          },
+        );
+
+        const newAccessToken = response.data.accessToken;
+        const newRefreshToken = response.data.refreshToken;
+
+        setCookie(COOKIE_KEYS.ACCESS_TOKEN, newAccessToken);
+        setCookie(COOKIE_KEYS.REFRESH_TOKEN, newRefreshToken);
+
+        onTokenRefreshed(newAccessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  },
 );
