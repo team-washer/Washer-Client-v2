59523cfc77937f640655545067e8b32f80029b15
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-07 14:13:57 +0900

    feat: env 변경

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
