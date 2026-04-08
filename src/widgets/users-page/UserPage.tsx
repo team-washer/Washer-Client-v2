1d2fb33c9f8359535422fae3f22a01b76fa95932
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-08 15:12:35 +0900

    fix: 사용하지 않는 구문 제거

--- src/widgets/users-page/UserPage.tsx
+++ src/widgets/users-page/UserPage.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
 import { managedUsersMock } from "@/entities/user/model/mock";
 import UserFilterPanel from "./ui/UserFilterPanel";
 import UserStatusPanel from "./ui/UserStatusPanel";
 
 export default function UsersPage() {
-  const [floor, setFloor] = useState<number | undefined>();
-  const [search, setSearch] = useState("");
-
   return (
     <div className="admin-page-grid xl:grid-cols-[1.9fr_0.62fr]">
       <div className="admin-page-item">
@@ -14,12 +10,7 @@ export default function UsersPage() {
       </div>
 
       <div className="admin-page-item">
-        <UserFilterPanel
-          floor={floor}
-          onFloorChange={setFloor}
-          search={search}
-          onSearchChange={setSearch}
-        />
+        <UserFilterPanel />
       </div>
     </div>
   );
