ff6e4cfb5004e4c2c2a2669666364e62bd33a625
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-04-29 00:06:37 +0900

    feat: rewrites 적용

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
-if (process.env.NODE_ENV === "development") {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
